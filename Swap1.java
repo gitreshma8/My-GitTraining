b6dcf0ca2cac3ad597ac2dcebcc0021af9b5fd7e
gitreshma8 <87260445+gitreshma8@users.noreply.github.com>
2021-07-11 21:39:05 +0530

    Line 13 updated

--- Swap1.java
+++ Swap1.java
@@ -10,7 +10,7 @@ public class Swap1{
 
 		System.out.println("Program for Swapping numbers");
 
-		System.out.println("Before swap");
+		System.out.println("Before swapping");
 		System.out.println("value of a" +a);
 		System.out.println("value of b" +b);
 		
